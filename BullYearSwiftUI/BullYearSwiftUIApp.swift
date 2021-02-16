e0204d218cbd3b5a479f3c7ff4b47e18dd521087
dsxsxsxs <jiacheng.shih@finc.com>
2021-02-16 15:48:55 +0900

    Initial Commit

---
+++ BullYearSwiftUI/BullYearSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullYearSwiftUIApp.swift
+//  BullYearSwiftUI
+//
+//  Created by Jiacheng Shih on 2021/02/16.
+//
+
+import SwiftUI
+
+@main
+struct BullYearSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BullYearSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BullYearSwiftUI
+//
+//  Created by Jiacheng Shih on 2021/02/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
